e86aa9e17f9e4b7437077ad944037873206ee15c
allencarne <34139066+allencarne@users.noreply.github.com>
2024-12-11 12:35:07 -0800

    Setting up Network Variable for player color

--- Assets/_Scipts/Player/Player.cs
+++ Assets/_Scipts/Player/Player.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
     // Components
     [SerializeField] CharacterCustomizationData customizationData;
@@ -12,41 +13,50 @@ public class Player : MonoBehaviour
 
     // Stats
     public float moveSpeed;
-
-    // Variables
     public int hairIndex;
+
+    NetworkVariable<Color> _bodyColor = new(writePerm: NetworkVariableWritePermission.Owner);
+    NetworkVariable<Color> _hairColor = new(writePerm: NetworkVariableWritePermission.Owner);
 
     private void Start()
     {
+        if (IsOwner)
+        {
         switch (PlayerPrefs.GetInt("SelectedCharacter"))
         {
             case 1:
-                Debug.Log("Selected Character 1");
-
                 PlayerPrefs.GetString("Character1Name");
                 body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character1SkinColor")];
-                //hair.sprite = customizationData.hairStyles[PlayerPrefs.GetInt("Character1HairStyle")];
                 hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character1HairColor")];
                 hairIndex = PlayerPrefs.GetInt("Character1HairStyle");
+
+                _bodyColor.Value = body.color;
+                _hairColor.Value = hair.color;
                 break;
             case 2:
-                Debug.Log("Selected Character 2");
-
                 PlayerPrefs.GetString("Character2Name");
                 body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character2SkinColor")];
-                //hair.sprite = customizationData.hairStyles[PlayerPrefs.GetInt("Character2HairStyle")];
                 hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character2HairColor")];
                 hairIndex = PlayerPrefs.GetInt("Character2HairStyle");
+
+                _bodyColor.Value = body.color;
+                _hairColor.Value = hair.color;
                 break;
             case 3:
-                Debug.Log("Selected Character 3");
-
                 PlayerPrefs.GetString("Character3Name");
                 body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character3SkinColor")];
-                //hair.sprite = customizationData.hairStyles[PlayerPrefs.GetInt("Character3HairStyle")];
                 hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character3HairColor")];
                 hairIndex = PlayerPrefs.GetInt("Character3HairStyle");
+
+                _bodyColor.Value = body.color;
+                _hairColor.Value = hair.color;
                 break;
+        }
+        }
+        else
+        {
+            body.color = _bodyColor.Value;
+            hair.color = _hairColor.Value;
         }
     }
 }
