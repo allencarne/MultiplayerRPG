106bbf7241ce88e05383e9df2a677b2451cc5b0b
allencarne <34139066+allencarne@users.noreply.github.com>
2024-12-13 21:42:53 -0800

    Can see other players healthbars

--- Assets/_Scipts/Player/Player.cs
+++ Assets/_Scipts/Player/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
     [SerializeField] GameObject cameraPrefab;
+    [SerializeField] Canvas playerUI;
 
     // Components
     [SerializeField] private CharacterCustomizationData customizationData;
@@ -39,47 +40,58 @@ public class Player : NetworkBehaviour
 
         if (IsOwner)
         {
-            // Set initial values based on the selected character
-            switch (PlayerPrefs.GetInt("SelectedCharacter"))
-            {
-                case 1:
-                    body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character1SkinColor")];
-                    hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character1HairColor")];
-                    hairIndex = PlayerPrefs.GetInt("Character1HairStyle");
-                    break;
-                case 2:
-                    body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character2SkinColor")];
-                    hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character2HairColor")];
-                    hairIndex = PlayerPrefs.GetInt("Character2HairStyle");
-                    break;
-                case 3:
-                    body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character3SkinColor")];
-                    hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character3HairColor")];
-                    hairIndex = PlayerPrefs.GetInt("Character3HairStyle");
-                    break;
-            }
-
-            // Update NetworkVariables
-            _bodyColor.Value = body.color;
-            _hairColor.Value = hair.color;
-
-            // Assign the camera to follow this player
-            if (Camera.main.GetComponent<CameraFollow>().playerTransform == null)
-            {
-                Camera.main.GetComponent<CameraFollow>().playerTransform = transform;
-            }
-            else
-            {
-                GameObject cameraInstance = Instantiate(cameraPrefab);
-                CameraFollow cameraFollow = cameraInstance.GetComponent<CameraFollow>();
-                cameraFollow.playerTransform = transform;
-            }
+            InitializeOwnerCharacter();
+            AssignPlayerCamera();
         }
         else
         {
             // Set initial colors for non-owner players
             body.color = _bodyColor.Value;
             hair.color = _hairColor.Value;
+        }
+    }
+
+    private void InitializeOwnerCharacter()
+    {
+        // Set initial values based on the selected character
+        switch (PlayerPrefs.GetInt("SelectedCharacter"))
+        {
+            case 1:
+                body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character1SkinColor")];
+                hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character1HairColor")];
+                hairIndex = PlayerPrefs.GetInt("Character1HairStyle");
+                break;
+            case 2:
+                body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character2SkinColor")];
+                hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character2HairColor")];
+                hairIndex = PlayerPrefs.GetInt("Character2HairStyle");
+                break;
+            case 3:
+                body.color = customizationData.skinColors[PlayerPrefs.GetInt("Character3SkinColor")];
+                hair.color = customizationData.hairColors[PlayerPrefs.GetInt("Character3HairColor")];
+                hairIndex = PlayerPrefs.GetInt("Character3HairStyle");
+                break;
+        }
+
+        // Update NetworkVariables
+        _bodyColor.Value = body.color;
+        _hairColor.Value = hair.color;
+    }
+
+    private void AssignPlayerCamera()
+    {
+        // Assign the camera to follow this player
+        if (Camera.main.GetComponent<CameraFollow>().playerTransform == null)
+        {
+            Camera.main.GetComponent<CameraFollow>().playerTransform = transform;
+        }
+        else
+        {
+            GameObject cameraInstance = Instantiate(cameraPrefab);
+            CameraFollow cameraFollow = cameraInstance.GetComponent<CameraFollow>();
+            cameraFollow.playerTransform = transform;
+
+            Camera cameraComponent = cameraInstance.GetComponent<Camera>();
         }
     }
 
