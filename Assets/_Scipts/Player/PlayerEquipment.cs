5a5755ab412ac84ca2d5184df4c58af1da5fca8b
allencarne <34139066+allencarne@users.noreply.github.com>
2024-12-29 16:41:13 -0800

    Started Player Equipment Sync

--- Assets/_Scipts/Player/PlayerEquipment.cs
+++ Assets/_Scipts/Player/PlayerEquipment.cs
@@ -1,11 +1,14 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerEquipment : MonoBehaviour
+public class PlayerEquipment : NetworkBehaviour
 {
     [SerializeField] SpriteRenderer Sword;
     [SerializeField] SpriteRenderer Staff;
     [SerializeField] SpriteRenderer Bow;
     [SerializeField] SpriteRenderer Dagger;
+
+    private NetworkVariable<CurrentWeapon> net_currentWeapon = new NetworkVariable<CurrentWeapon>(writePerm: NetworkVariableWritePermission.Owner);
 
     public enum CurrentWeapon
 	{
@@ -18,7 +21,26 @@ public class PlayerEquipment : MonoBehaviour
 
 	public CurrentWeapon currentWeapon = CurrentWeapon.None;
 
-	public void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    public override void OnNetworkSpawn()
+    {
+        net_currentWeapon.OnValueChanged += OnWeaponChanged;
+
+        if (IsOwner)
+        {
+            net_currentWeapon.Value = currentWeapon;
+        }
+        else
+        {
+            currentWeapon = net_currentWeapon.Value;
+        }
+    }
+
+    public override void OnDestroy()
+    {
+        net_currentWeapon.OnValueChanged -= OnWeaponChanged;
+    }
+
+    public void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
 	{
 		if (newItem != null)
 		{
@@ -49,18 +71,30 @@ public class PlayerEquipment : MonoBehaviour
         switch (newWeapon.weaponType)
         {
             case WeaponType.Sword:
+                currentWeapon = CurrentWeapon.Sword;
+                net_currentWeapon.Value = currentWeapon;
+
                 Sword.enabled = true;
                 Sword.sprite = newWeapon.weaponSprite;
                 break;
             case WeaponType.Staff:
+                currentWeapon = CurrentWeapon.Staff;
+                net_currentWeapon.Value = currentWeapon;
+
                 Staff.enabled = true;
                 Staff.sprite = newWeapon.weaponSprite;
                 break;
             case WeaponType.Bow:
+                currentWeapon = CurrentWeapon.Bow;
+                net_currentWeapon.Value = currentWeapon;
+
                 Bow.enabled = true;
                 Bow.sprite = newWeapon.weaponSprite;
                 break;
             case WeaponType.Dagger:
+                currentWeapon = CurrentWeapon.Dagger;
+                net_currentWeapon.Value = currentWeapon;
+
                 Dagger.enabled = true;
                 Dagger.sprite = newWeapon.weaponSprite;
                 break;
@@ -72,17 +106,34 @@ public class PlayerEquipment : MonoBehaviour
         switch (oldWeapon.weaponType)
         {
             case WeaponType.Sword:
+                currentWeapon = CurrentWeapon.None;
+                net_currentWeapon.Value = currentWeapon;
+
                 Sword.enabled = false;
                 break;
             case WeaponType.Staff:
+                currentWeapon = CurrentWeapon.None;
+                net_currentWeapon.Value = currentWeapon;
+
                 Staff.enabled = false;
                 break;
             case WeaponType.Bow:
+                currentWeapon = CurrentWeapon.None;
+                net_currentWeapon.Value = currentWeapon;
+
                 Bow.enabled = false;
                 break;
             case WeaponType.Dagger:
+                currentWeapon = CurrentWeapon.None;
+                net_currentWeapon.Value = currentWeapon;
+
                 Dagger.enabled = false;
                 break;
         }
+    }
+
+    void OnWeaponChanged(CurrentWeapon previousWeapon, CurrentWeapon newWeapon)
+    {
+        currentWeapon = newWeapon;
     }
 }
