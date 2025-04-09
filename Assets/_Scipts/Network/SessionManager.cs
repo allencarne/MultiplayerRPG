a1c887a4df0f63791d0c4214cae8cc50a40f6b09
allencarne <34139066+allencarne@users.noreply.github.com>
2025-04-08 20:11:04 -0700

    Added Lobby

---
+++ Assets/_Scipts/Network/SessionManager.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+
+public class SessionManager : MonoBehaviour
+{
+    [SerializeField] GameObject playButton;
+
+    private Lobby _connectedLobby;
+    private QueryResponse _lobbies;
+    private UnityTransport _transport;
+    private const string JoinCodeKey = "j";
+    private string _playerId;
+
+    private void Start()
+    {
+        playButton.SetActive(true);
+    }
+
+    private void Awake() => _transport = FindAnyObjectByType<UnityTransport>();
+
+    public async void CreateOrJoinLobby()
+    {
+        await Authenticate();
+
+        _connectedLobby = await QuickJoinLobby() ?? await CreateLobby();
+
+        if (_connectedLobby != null) playButton.SetActive(false);
+    }
+
+    private async Task Authenticate()
+    {
+        var options = new InitializationOptions();
+
+        await UnityServices.InitializeAsync(options);
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        _playerId = AuthenticationService.Instance.PlayerId;
+    }
+
+    private async Task<Lobby> QuickJoinLobby()
+    {
+        try
+        {
+            var lobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+            var a = await RelayService.Instance.JoinAllocationAsync(lobby.Data[JoinCodeKey].Value);
+
+            var relayServerData = AllocationUtils.ToRelayServerData(a, "wss");
+            _transport.SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
+            return lobby;
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"No lobbies available via quick join");
+            return null;
+        }
+    }
+
+    private async Task<Lobby> CreateLobby()
+    {
+        try
+        {
+            const int maxPlayers = 100;
+
+            var a = await RelayService.Instance.CreateAllocationAsync(maxPlayers);
+            var joinCode = await RelayService.Instance.GetJoinCodeAsync(a.AllocationId);
+
+            var options = new CreateLobbyOptions
+            {
+                Data = new Dictionary<string, DataObject> { { JoinCodeKey, new DataObject(DataObject.VisibilityOptions.Public, joinCode) } }
+            };
+            var lobby = await LobbyService.Instance.CreateLobbyAsync("Useless Lobby Name", maxPlayers, options);
+
+            StartCoroutine(HeartBeatLobbyCoroutine(lobby.Id, 15));
+
+            var relayServerData = AllocationUtils.ToRelayServerData(a, "wss");
+            _transport.SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+            return lobby;
+        }
+        catch (Exception e)
+        {
+            Debug.LogFormat("Failed creating a lobby");
+            return null;
+        }
+    }
+
+    private void SetTransformAsClient(JoinAllocation a)
+    {
+        _transport.SetClientRelayData(a.RelayServer.IpV4, (ushort)a.RelayServer.Port, a.AllocationIdBytes, a.Key, a.ConnectionData, a.HostConnectionData);
+    }
+
+    private static IEnumerator HeartBeatLobbyCoroutine(string lobbyId, float waitTimeSeconds)
+    {
+        var delay = new WaitForSecondsRealtime(waitTimeSeconds);
+        while (true)
+        {
+            LobbyService.Instance.SendHeartbeatPingAsync(lobbyId);
+            yield return delay;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        try
+        {
+            StopAllCoroutines();
+            if (_connectedLobby != null)
+            {
+                if (_connectedLobby.HostId == _playerId)
+                    LobbyService.Instance.DeleteLobbyAsync(_connectedLobby.Id);
+                else
+                    LobbyService.Instance.RemovePlayerAsync(_connectedLobby.Id, _playerId);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Error shutting down lobby: {e}");
+        }
+    }
+}
