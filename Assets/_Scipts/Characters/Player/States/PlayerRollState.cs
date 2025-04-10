dd706b74479bd84f46b0f86526af70153ad272b6
allencarne <34139066+allencarne@users.noreply.github.com>
2025-04-10 13:44:35 -0700

    Fixed Roll Animations

--- Assets/_Scipts/Characters/Player/States/PlayerRollState.cs
+++ Assets/_Scipts/Characters/Player/States/PlayerRollState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerRollState : PlayerState
 {
@@ -10,27 +11,10 @@ public class PlayerRollState : PlayerState
         owner.StartCoroutine(Duration(owner));
 
         Vector2 moveInput = owner.InputHandler.MoveInput.normalized;
-        Vector2 direction;
-        if (moveInput != Vector2.zero)
-        {
-            direction = moveInput;
-        }
-        else
-        {
-            // Fall back to the direction the player is currently facing
-            float horizontal = owner.BodyAnimator.GetFloat("Horizontal");
-            float vertical = owner.BodyAnimator.GetFloat("Vertical");
-            direction = new Vector2(horizontal, vertical).normalized;
-
-            // default to right if no direction
-            if (direction == Vector2.zero)
-            {
-                direction = Vector2.right;
-            }
-        }
+        Vector2 direction = owner.SnapDirection(moveInput);
 
         // Apply force
-        owner.PlayerRB.AddForce(direction * 25, ForceMode2D.Impulse);
+        owner.PlayerRB.AddForce(moveInput * 25, ForceMode2D.Impulse);
 
         // Animate
         owner.BodyAnimator.SetFloat("Horizontal", direction.x);
