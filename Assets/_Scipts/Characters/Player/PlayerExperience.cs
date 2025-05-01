badeab7086161c2fa052c1cbb27a7cae803fa613
allencarne <34139066+allencarne@users.noreply.github.com>
2025-05-01 12:17:16 -0700

    Player Level System

--- Assets/_Scipts/Characters/Player/PlayerExperience.cs
+++ Assets/_Scipts/Characters/Player/PlayerExperience.cs
@@ -1,77 +1,124 @@
 using System.Collections;
+using System.Xml.Linq;
+using TMPro;
 using Unity.Netcode;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class PlayerExperience : NetworkBehaviour
 {
+    [Header("UI")]
     [SerializeField] RectTransform rect;
-
-    [SerializeField] Player player;
-    [SerializeField] Image expBar;
-    [SerializeField] Image expBar_Back;
-
-    [SerializeField] GameObject levelUpParticle;
-    [SerializeField] GameObject levelUpParticle_Back;
+    [SerializeField] TextMeshProUGUI experienceText;
+    [SerializeField] TextMeshProUGUI levelText;
     [SerializeField] GameObject levelUpText;
 
-    Coroutine lerp;
+    [Header("Components")]
+    [SerializeField] Player player;
+    [SerializeField] Image frontXpBar;
+    [SerializeField] Image backXpBar;
 
-    private void Start()
+    [Header("Particles")]
+    [SerializeField] GameObject levelUpParticle;
+    [SerializeField] GameObject levelUpParticle_Back;
+
+    [Header("Variables")]
+    private float lerpTimer;
+    private float delayTimer;
+
+    [Header("Multipliers")]
+    [Range(1f, 300f)]
+    public float additionMultiplier = 300;
+    [Range(2f, 4f)]
+    public float powerMultiplier = 2;
+    [Range(7f, 14f)]
+    public float divisionMultiplier = 7;
+
+    void Start()
     {
-        UpdateEXPBar(player.CurrentExperience, player.RequiredExperience);
+        frontXpBar.fillAmount = player.CurrentExperience / player.RequiredExperience;
+        backXpBar.fillAmount = player.CurrentExperience / player.RequiredExperience;
+
+        player.RequiredExperience = CalculateRequiredXp();
+
+        levelText.text = player.PlayerLevel.ToString();
     }
 
-    public void IncreaseEXP(float amout)
+    void Update()
     {
-        player.CurrentExperience += amout;
+        UpdateXpUI();
 
         if (player.CurrentExperience >= player.RequiredExperience)
         {
             LevelUp();
         }
-
-        UpdateEXPBar(player.CurrentExperience, player.RequiredExperience);
     }
 
-    void LevelUp()
+    public void UpdateXpUI()
     {
-        SpawnEffectClientRPC();
+        float xpFraction = player.CurrentExperience / player.RequiredExperience;
+        float FXP = frontXpBar.fillAmount;
+        if (FXP < xpFraction)
+        {
+            delayTimer += Time.deltaTime;
+            backXpBar.fillAmount = xpFraction;
+            if (delayTimer > 0.5)
+            {
+                lerpTimer += Time.deltaTime;
+                float percentComplete = lerpTimer / 4;
+                frontXpBar.fillAmount = Mathf.Lerp(FXP, backXpBar.fillAmount, percentComplete);
+            }
+        }
 
+        experienceText.text = player.CurrentExperience + "/" + player.RequiredExperience;
+    }
+
+    private int CalculateRequiredXp()
+    {
+        int solveForRequiredXp = 0;
+        for (int levelCycle = 1; levelCycle <= player.PlayerLevel; levelCycle++)
+        {
+            solveForRequiredXp += (int)Mathf.Floor(levelCycle + additionMultiplier * Mathf.Pow(powerMultiplier, levelCycle / divisionMultiplier));
+        }
+        return solveForRequiredXp / 4;
+    }
+
+    public void IncreaseEXP(float xpGained)
+    {
+        //ShowFloatingText(xpGained, expText);
+
+        player.CurrentExperience += xpGained;
+        lerpTimer = 0f;
+        delayTimer = 0f;
+    }
+
+    public void LevelUp()
+    {
+        // Increase Player Level
         player.PlayerLevel++;
-        player.CurrentExperience -= player.RequiredExperience;
-        player.RequiredExperience += 5;
+        levelText.text = player.PlayerLevel.ToString();
 
-        if (player.CurrentExperience >= player.RequiredExperience)
-        {
-            LevelUp();
-        }
+        // Increase Player Health
+        //player.MaxHealth++;
+        //float missingHealth = player.MaxHealth - player.Health;
+        //player.Heal(missingHealth, false);
+
+        // Increase Player Damage
+        //player.BaseDamage++;
+        //player.CurrentDamage++;
+
+        // Update Bar
+        frontXpBar.fillAmount = 0f;
+        backXpBar.fillAmount = 0f;
+        player.CurrentExperience = Mathf.RoundToInt(player.CurrentExperience - player.RequiredExperience);
+        player.RequiredExperience = CalculateRequiredXp();
+
+        // Effects
+        SpawnEffectClientRPC();
     }
 
-    public void UpdateEXPBar(float currentEXP, float reqEXP)
-    {
-        if (reqEXP <= 0) return;
-
-        expBar_Back.fillAmount = currentEXP / reqEXP;
-
-        if (lerp != null)
-        {
-            StopCoroutine(lerp);
-        }
-        lerp = StartCoroutine(LerpBar(currentEXP / reqEXP));
-    }
-
-    IEnumerator LerpBar(float targetFillAmount)
-    {
-        float currentFillAmount = expBar.fillAmount;
-        while (!Mathf.Approximately(currentFillAmount, targetFillAmount))
-        {
-            currentFillAmount = Mathf.Lerp(currentFillAmount, targetFillAmount, 5f * Time.deltaTime);
-            expBar.fillAmount = currentFillAmount;
-            yield return null;
-        }
-    }
 
     [ClientRpc]
     void SpawnEffectClientRPC()
