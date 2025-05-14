eedeae5714f0378fb3514e0737b5dbc70d0e10e8
allencarne <34139066+allencarne@users.noreply.github.com>
2025-05-14 08:02:24 -0700

    Beach Hut Inside working

--- Assets/_Scipts/Misc/Building.cs
+++ Assets/_Scipts/Misc/Building.cs
@@ -6,21 +6,23 @@ public class Building : MonoBehaviour
     [SerializeField] GameObject inside;
     [SerializeField] GameObject outside;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            inside.SetActive(true);
-            outside.SetActive(false);
-        }
-    }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            inside.SetActive(false);
-            outside.SetActive(true);
+            Vector2 playerPos = collision.transform.position;
+
+            if (playerPos.y > transform.position.y)
+            {
+                inside.SetActive(true);
+                outside.SetActive(false);
+            }
+
+            if (playerPos.y < transform.position.y)
+            {
+                inside.SetActive(false);
+                outside.SetActive(true);
+            }
         }
     }
 }
