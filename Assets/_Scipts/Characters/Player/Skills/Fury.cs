8affc949c42175793d859760073dc6eb8f412093
allencarne <34139066+allencarne@users.noreply.github.com>
2025-05-14 20:16:47 -0700

    Fury decrease over time

--- Assets/_Scipts/Characters/Player/Skills/Fury.cs
+++ Assets/_Scipts/Characters/Player/Skills/Fury.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using Unity.Netcode;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fury : PlayerAbility
 {
+    float idleTime;
+    Coroutine timer;
+
     public override void StartAbility(PlayerStateMachine owner)
     {
         DamageOnTrigger.OnDamageDealt.AddListener(GainFury);
@@ -11,7 +14,29 @@ public class Fury : PlayerAbility
 
     public override void UpdateAbility(PlayerStateMachine owner)
     {
+        idleTime += Time.deltaTime;
 
+        if (idleTime >= 8 && owner.player.Fury.Value > 0)
+        {
+            if (timer == null)
+            {
+                timer = StartCoroutine(DecreaseFury(owner));
+            }
+        }
+        else if (timer != null)
+        {
+            StopCoroutine(DecreaseFury(owner));
+            timer = null;
+        }
+    }
+
+    IEnumerator DecreaseFury(PlayerStateMachine owner)
+    {
+        while (owner.player.Fury.Value > 0 && idleTime >= 8)
+        {
+            owner.player.Fury.Value--;
+            yield return new WaitForSeconds(1f);
+        }
     }
 
     public override void FixedUpdateAbility(PlayerStateMachine owner)
@@ -25,6 +50,13 @@ public class Fury : PlayerAbility
         if (owner != null)
         {
             owner.player.Fury.Value = Mathf.Min(owner.player.Fury.Value + 5, owner.player.MaxFury.Value);
+            idleTime = 0;
+
+            if (timer != null)
+            {
+                StopCoroutine(timer);
+                timer = null;
+            }
         }
     }
 
