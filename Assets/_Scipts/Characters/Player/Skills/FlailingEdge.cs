31b40efd7c40617269e3a6dc8baa6cefe2729c6c
allencarne <34139066+allencarne@users.noreply.github.com>
2025-05-17 19:22:54 -0700

    Flailing Edge Added

---
+++ Assets/_Scipts/Characters/Player/Skills/FlailingEdge.cs
@@ -0,0 +1,89 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class FlailingEdge : PlayerAbility
+{
+    [SerializeField] GameObject attackPrefab;
+    [SerializeField] int abilityDamage;
+    [SerializeField] float attackRange;
+
+    [Header("Time")]
+    [SerializeField] float castTime;
+    [SerializeField] float recoveryTime;
+    [SerializeField] float coolDown;
+
+    Vector2 aimDirection;
+    Quaternion aimRotation;
+
+    public override void StartAbility(PlayerStateMachine owner)
+    {
+        owner.CanOffensive = false;
+        owner.IsAttacking = true;
+
+        // Set Variables
+        aimDirection = owner.Aimer.right;
+        aimRotation = owner.Aimer.rotation;
+        Vector2 snappedDirection = owner.SnapDirection(aimDirection);
+
+        // Stop
+        owner.PlayerRB.linearVelocity = Vector2.zero;
+
+        // Animate
+        owner.AnimateCast(snappedDirection);
+
+        // Cast Bar
+        owner.StartCast(castTime);
+
+        // Timers
+        StartCoroutine(owner.CastTime(castTime, recoveryTime, this));
+        StartCoroutine(owner.CoolDownTime(PlayerStateMachine.SkillType.Offensive, coolDown));
+    }
+
+    public override void UpdateAbility(PlayerStateMachine owner)
+    {
+        owner.HandlePotentialInterrupt();
+    }
+
+    public override void FixedUpdateAbility(PlayerStateMachine owner)
+    {
+
+    }
+
+    public override void Impact(PlayerStateMachine owner)
+    {
+        if (IsServer)
+        {
+            SpawnAttack(transform.position, aimRotation, aimDirection, OwnerClientId, owner.player.CurrentDamage.Value);
+        }
+        else
+        {
+            AttackServerRpc(transform.position, aimRotation, aimDirection, OwnerClientId, owner.player.CurrentDamage.Value);
+        }
+    }
+
+    void SpawnAttack(Vector2 spawnPosition, Quaternion spawnRotation, Vector2 aimDirection, ulong attackerID, int attackerDamage)
+    {
+        NetworkObject Attacker = NetworkManager.Singleton.ConnectedClients[attackerID].PlayerObject;
+
+        Vector2 offset = aimDirection.normalized * attackRange;
+
+        GameObject attackInstance = Instantiate(attackPrefab, spawnPosition + offset, spawnRotation);
+        NetworkObject attackNetObj = attackInstance.GetComponent<NetworkObject>();
+
+        attackNetObj.Spawn();
+
+        DamageOnTrigger damageOnTrigger = attackInstance.GetComponent<DamageOnTrigger>();
+        if (damageOnTrigger != null)
+        {
+            damageOnTrigger.attacker = Attacker;
+            damageOnTrigger.AbilityDamage = abilityDamage;
+            damageOnTrigger.CharacterDamage = attackerDamage;
+        }
+    }
+
+    [ServerRpc]
+    void AttackServerRpc(Vector2 spawnPosition, Quaternion spawnRotation, Vector2 aimDirection, ulong attackerID, int attackerDamage)
+    {
+        SpawnAttack(spawnPosition, spawnRotation, aimDirection, attackerID, attackerDamage);
+    }
+}
