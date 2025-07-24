c9d09207c008c9a8e23906b45deffa8606f2158c
allencarne <34139066+allencarne@users.noreply.github.com>
2025-07-24 06:46:21 -0700

    Dynamic Quest Rewards

--- Assets/_Scipts/UI/PlayerUI/QuestUI.cs
+++ Assets/_Scipts/UI/PlayerUI/QuestUI.cs
@@ -1,12 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestUI : MonoBehaviour
 {
-    [SerializeField] GameObject QuestListUI;
     [SerializeField] Quest[] allQuests;
 
+    [SerializeField] GameObject QuestListUI;
     [SerializeField] GameObject QuestUI_Button;
+
+    [SerializeField] GameObject RewardListUI;
+    [SerializeField] GameObject RewardUI_Item;
 
     [SerializeField] TextMeshProUGUI questName;
     [SerializeField] TextMeshProUGUI questInfo;
@@ -15,6 +19,8 @@ public class QuestUI : MonoBehaviour
 
     private void Start()
     {
+        ShowQuestDetails(allQuests[0]);
+
         foreach (Quest quest in allQuests)
         {
             GameObject listItem = Instantiate(QuestUI_Button, QuestListUI.transform);
@@ -39,5 +45,21 @@ public class QuestUI : MonoBehaviour
         questInfo.text = quest.instructions;
         goldReward.text = quest.goldReward.ToString();
         expReward.text = quest.expReward.ToString();
+
+        foreach (Transform child in RewardListUI.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Item reward in quest.reward)
+        {
+            GameObject itmeUI = Instantiate(RewardUI_Item, RewardListUI.transform);
+
+            Image image = itmeUI.GetComponent<Image>();
+            if (image != null)
+            {
+                image.sprite = reward.Icon;
+            }
+        }
     }
 }
