ffc812c427a8bcf1f98a2e3e3d21a8ec7d7fba6e
allencarne <34139066+allencarne@users.noreply.github.com>
2025-07-25 07:41:27 -0700

    NPC Quest UI Info
    Plugged in text objects to get quest dialogue/name

--- Assets/_Scipts/Characters/NPC/NPCQuest.cs
+++ Assets/_Scipts/Characters/NPC/NPCQuest.cs
@@ -1,21 +1,39 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class NPCQuest : MonoBehaviour
 {
+    public Quest[] Quests;
+
+    [Header("Text")]
+    [SerializeField] TextMeshProUGUI questTitle;
+    [SerializeField] TextMeshProUGUI questInfo;
+    [SerializeField] TextMeshProUGUI questGold;
+    [SerializeField] TextMeshProUGUI questEXP;
+
+    [Header("Buttons")]
     [SerializeField] Button AcceptButton;
     [SerializeField] Button DeclineButton;
 
-    public Quest[] Quests;
     [SerializeField] GameObject QuestUI;
     Player playerReference;
+    int questIndex;
 
     public void StartQuest(Player player)
     {
         QuestUI.SetActive(true);
-        //UpdateDialogueText();
+        UpdateQuestInfo(Quests[questIndex]);
         if (EventSystem.current != null) EventSystem.current.SetSelectedGameObject(AcceptButton.gameObject);
         if (playerReference == null) playerReference = player;
+    }
+
+    void UpdateQuestInfo(Quest quest)
+    {
+        questTitle.text = quest.questName;
+        questInfo.text = quest.instructions;
+        questGold.text = quest.goldReward.ToString();
+        questEXP.text = quest.expReward.ToString();
     }
 }
