831be830d034fcb063896c0bbf1d9345c063db74
allencarne <34139066+allencarne@users.noreply.github.com>
2025-08-05 20:33:05 -0700

    Drag and Drop Inventory Items

--- Assets/_Scipts/UI/Inventory/InventorySlot.cs
+++ Assets/_Scipts/UI/Inventory/InventorySlot.cs
@@ -1,9 +1,9 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using WebSocketSharp;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public EquipmentManager equipmentManager;
     public Inventory inventory;
@@ -57,6 +57,55 @@ public class InventorySlot : MonoBehaviour
     {
         if (amountText != null)
         {
+            amountText.text = "";
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // Get drag source
+        ItemDrag draggedItem = eventData.pointerDrag?.GetComponent<ItemDrag>();
+        if (draggedItem == null) return;
+
+        InventorySlot fromSlot = draggedItem.inventorySlot;
+        InventorySlot toSlot = this;
+
+        // Don't allow dropping onto self
+        if (fromSlot == toSlot) return;
+
+        // Get their slot data
+        InventorySlotData fromData = fromSlot.slotData;
+        InventorySlotData toData = toSlot.slotData;
+
+        // Swap logic
+        fromSlot.slotData = toData;
+        toSlot.slotData = fromData;
+
+        // Update Inventory
+        fromSlot.inventory.items[fromSlot.slotIndex] = fromSlot.slotData;
+        toSlot.inventory.items[toSlot.slotIndex] = toSlot.slotData;
+
+        // Update visuals
+        fromSlot.UpdateSlotVisuals();
+        toSlot.UpdateSlotVisuals();
+
+        // Save
+        fromSlot.inventory.initialize.SaveInventory(fromSlot.slotData?.item, fromSlot.slotIndex, fromSlot.slotData?.quantity ?? 0);
+        toSlot.inventory.initialize.SaveInventory(toSlot.slotData?.item, toSlot.slotIndex, toSlot.slotData?.quantity ?? 0);
+    }
+
+    public void UpdateSlotVisuals()
+    {
+        if (slotData != null)
+        {
+            icon.sprite = slotData.item.Icon;
+            icon.color = Color.white;
+            amountText.text = (slotData.quantity > 1) ? slotData.quantity.ToString() : "";
+        }
+        else
+        {
+            icon.sprite = null;
+            icon.color = Color.black;
             amountText.text = "";
         }
     }
--- Assets/_Scipts/UI/Inventory/ItemDrag.cs
+++ Assets/_Scipts/UI/Inventory/ItemDrag.cs
@@ -1,35 +1,51 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemDrag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    [SerializeField] RectTransform rectTransform;
-    [SerializeField] InventorySlot inventorySlot;
-    Canvas canvas;
-    CanvasGroup canvasGroup;
+    public Image image;
+    public InventorySlot inventorySlot;
+
+    private Transform originalParent;
+    private Canvas rootCanvas;
 
     private void Awake()
     {
         inventorySlot = GetComponentInParent<InventorySlot>();
-        canvas = GetComponentInParent<Canvas>();
-        canvasGroup = GetComponent<CanvasGroup>();
+        rootCanvas = GetComponentInParent<Canvas>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        canvasGroup.blocksRaycasts = false;
+        image.raycastTarget = false;
+
+        // Store the original parent and move to top-level canvas to render above all
+        originalParent = image.transform.parent;
+        image.transform.SetParent(rootCanvas.transform, true);
+
+        // Set background image to solid black
+        // Hide Text_Stack
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         RectTransformUtility.ScreenPointToWorldPointInRectangle(
-            (RectTransform)transform.parent, eventData.position, eventData.pressEventCamera, out Vector3 worldPoint);
+            (RectTransform)rootCanvas.transform, eventData.position, eventData.pressEventCamera, out Vector3 worldPoint);
 
-        transform.position = worldPoint;
+        image.transform.position = worldPoint;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.blocksRaycasts = true;
+        image.raycastTarget = true;
+
+        // Return to original parent
+        image.transform.SetParent(originalParent, true);
+
+        // Reset position and put it on top of siblings (front of UI)
+        image.transform.localScale = Vector3.one;
+        image.transform.localPosition = Vector3.zero;
+        image.transform.SetAsFirstSibling();
     }
 }
