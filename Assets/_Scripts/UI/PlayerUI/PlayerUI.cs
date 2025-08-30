4b947b499818dc37b79ca7f20c4c4e7160c38923
allencarne <34139066+allencarne@users.noreply.github.com>
2025-08-30 12:24:21 -0700

    Controller Nav improved

--- Assets/_Scripts/UI/PlayerUI/PlayerUI.cs
+++ Assets/_Scripts/UI/PlayerUI/PlayerUI.cs
@@ -6,7 +6,13 @@ using UnityEngine.InputSystem;
 public class PlayerUI : NetworkBehaviour
 {
     [SerializeField] PlayerInput playerInput;
+
+    [Header("FirstSelected")]
     [SerializeField] GameObject inventoryfirstselected;
+    [SerializeField] GameObject skillfirstselected;
+    [SerializeField] GameObject attributefirstselected;
+    [SerializeField] GameObject questfirstselected;
+    [SerializeField] GameObject settingsfirstselected;
 
     [Header("Panel")]
     [SerializeField] GameObject inventoryPanel;
@@ -35,39 +41,95 @@ public class PlayerUI : NetworkBehaviour
 
     public void _InventoryUI()
     {
-        inventoryPanel.SetActive(!inventoryPanel.activeSelf);
-
-        if (inventoryPanel.activeInHierarchy && UsingGamepad())
+        if (inventoryPanel.activeSelf)
         {
-            EventSystem.current.SetSelectedGameObject(inventoryfirstselected);
+            inventoryPanel.SetActive(false);
+            UpdateSelectedUI();
         }
-
-        if (!inventoryPanel.activeInHierarchy) EventSystem.current.SetSelectedGameObject(null);
+        else
+        {
+            inventoryPanel.SetActive(true);
+            if (UsingGamepad()) EventSystem.current.SetSelectedGameObject(inventoryfirstselected);
+        }
     }
 
     public void _SkillUI()
     {
-        skillPanel.SetActive(!skillPanel.activeSelf);
+        if (skillPanel.activeSelf)
+        {
+            skillPanel.SetActive(false);
+            UpdateSelectedUI();
+        }
+        else
+        {
+            skillPanel.SetActive(true);
+            if (UsingGamepad()) EventSystem.current.SetSelectedGameObject(skillfirstselected);
+        }
     }
 
     public void _AttributeUI()
     {
-        attributePanel.SetActive(!attributePanel.activeSelf);
+        if (attributePanel.activeSelf)
+        {
+            attributePanel.SetActive(false);
+            UpdateSelectedUI();
+        }
+        else
+        {
+            attributePanel.SetActive(true);
+            if (UsingGamepad()) EventSystem.current.SetSelectedGameObject(attributefirstselected);
+        }
     }
 
     public void _QuestLogUI()
     {
-        questLogPanel.SetActive(!questLogPanel.activeSelf);
+        if (questLogPanel.activeSelf)
+        {
+            questLogPanel.SetActive(false);
+            UpdateSelectedUI();
+        }
+        else
+        {
+            questLogPanel.SetActive(true);
+            if (UsingGamepad()) EventSystem.current.SetSelectedGameObject(questfirstselected);
+        }
     }
 
     public void _SettingsUI()
     {
-        settingsPanel.SetActive(!settingsPanel.activeSelf);
+        if (settingsPanel.activeSelf)
+        {
+            settingsPanel.SetActive(false);
+            UpdateSelectedUI();
+        }
+        else
+        {
+            settingsPanel.SetActive(true);
+            if (UsingGamepad()) EventSystem.current.SetSelectedGameObject(settingsfirstselected);
+        }
     }
 
     private bool UsingGamepad()
     {
-        // PlayerInput keeps track of control scheme
         return playerInput != null && playerInput.currentControlScheme == "Gamepad";
+    }
+
+    void UpdateSelectedUI()
+    {
+        if (!UsingGamepad())
+            return;
+
+        if (inventoryPanel.activeSelf)
+            EventSystem.current.SetSelectedGameObject(inventoryfirstselected);
+        else if (skillPanel.activeSelf)
+            EventSystem.current.SetSelectedGameObject(skillfirstselected);
+        else if (attributePanel.activeSelf)
+            EventSystem.current.SetSelectedGameObject(attributefirstselected);
+        else if (questLogPanel.activeSelf)
+            EventSystem.current.SetSelectedGameObject(questfirstselected);
+        else if (settingsPanel.activeSelf)
+            EventSystem.current.SetSelectedGameObject(settingsfirstselected);
+        else
+            EventSystem.current.SetSelectedGameObject(null);
     }
 }
