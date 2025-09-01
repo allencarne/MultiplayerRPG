8d52c7d7036706df04eec926aa09c925d5bc0f93
allencarne <34139066+allencarne@users.noreply.github.com>
2025-09-01 08:19:53 -0700

    MiniMap Sliders Added

---
+++ Assets/_Scripts/UI/PlayerUI/MiniMap.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniMap : MonoBehaviour
+{
+    [SerializeField] private Camera mapCamera;
+    [SerializeField] private RawImage map;
+    [SerializeField] private Slider zoomSlider;
+    [SerializeField] private Slider alphaSlider;
+    float minZoom = 11.25f;
+    float maxZoom = 60f;
+
+    private void Start()
+    {
+        // Set slider ranges
+        zoomSlider.minValue = 0f;
+        zoomSlider.maxValue = 1f;
+        zoomSlider.value = .75f;
+
+        alphaSlider.minValue = 0f;
+        alphaSlider.maxValue = 1f;
+        alphaSlider.value = .75f;
+
+        // Hook up events
+        zoomSlider.onValueChanged.AddListener(OnZoomChanged);
+        alphaSlider.onValueChanged.AddListener(OnAlphaChanged);
+
+        // Initialize
+        OnZoomChanged(zoomSlider.value);
+        OnAlphaChanged(alphaSlider.value);
+    }
+
+    private void OnZoomChanged(float value)
+    {
+        // Lerp between minZoom and maxZoom
+        float zoom = Mathf.Lerp(minZoom, maxZoom, value);
+        mapCamera.orthographicSize = zoom;
+    }
+
+    private void OnAlphaChanged(float value)
+    {
+        Color c = map.color;
+        c.a = value;
+        map.color = c;
+    }
+}
