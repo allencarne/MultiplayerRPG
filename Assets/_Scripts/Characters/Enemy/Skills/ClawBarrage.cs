a3755f2a425295532e41facaff9a12eb166d7d63
allencarne <34139066+allencarne@users.noreply.github.com>
2025-10-04 07:23:06 -0700

    Added Claw Barrage Ability

---
+++ Assets/_Scripts/Characters/Enemy/Skills/ClawBarrage.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class ClawBarrage : EnemyAbility
+{
+    public override void AbilityStart(EnemyStateMachine owner)
+    {
+        InitializeAbility(skillType, owner);
+        owner.EnemyRB.linearVelocity = Vector2.zero;
+        ModifiedCastTime = CastTime / owner.enemy.CurrentAttackSpeed;
+        SpawnPosition = owner.transform.position;
+
+        // Aim
+        AimDirection = (owner.Target.position - transform.position).normalized;
+        float angle = Mathf.Atan2(AimDirection.y, AimDirection.x) * Mathf.Rad2Deg;
+        AimRotation = Quaternion.Euler(0, 0, angle);
+        AimOffset = AimDirection.normalized * AttackRange_;
+
+        ChangeState(State.Cast, ModifiedCastTime);
+        CastState(owner);
+    }
+
+    public override void AbilityUpdate(EnemyStateMachine owner)
+    {
+        if (currentState == State.Done) return;
+
+        stateTimer -= Time.deltaTime;
+        if (stateTimer <= 0f) StateTransition(owner);
+    }
+
+    public override void AbilityFixedUpdate(EnemyStateMachine owner)
+    {
+
+    }
+
+    public override void CastState(EnemyStateMachine owner)
+    {
+        AnimateEnemy(owner, skillType, State.Cast);
+        owner.EnemyAnimator.SetFloat("Horizontal", AimDirection.x);
+        owner.EnemyAnimator.SetFloat("Vertical", AimDirection.y);
+
+        owner.enemy.CastBar.StartCast(CastTime, owner.enemy.CurrentAttackSpeed);
+        Telegraph(true, false);
+    }
+
+    public override void ImpactState(EnemyStateMachine owner)
+    {
+        owner.Buffs.phase.StartPhase(5);
+        owner.Buffs.protection.StartProtection(2, 5);
+
+        AnimateEnemy(owner, skillType, State.Impact);
+        Attack(owner.NetworkObject, true, false);
+    }
+
+    public override void RecoveryState(EnemyStateMachine owner)
+    {
+        AnimateEnemy(owner, skillType, State.Recovery);
+        owner.enemy.CastBar.StartRecovery(RecoveryTime, owner.enemy.CurrentAttackSpeed);
+    }
+}
---
+++ Assets/_Scripts/Characters/Enemy/Skills/CrabSmash.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class CrabSmash : EnemyAbility
+{
+    [Header("Slide")]
+    [SerializeField] float slideForce;
+    [SerializeField] float buffDuration;
+
+    public override void AbilityStart(EnemyStateMachine owner)
+    {
+        InitializeAbility(skillType, owner);
+        owner.EnemyRB.linearVelocity = Vector2.zero;
+        ModifiedCastTime = CastTime / owner.enemy.CurrentAttackSpeed;
+        SpawnPosition = owner.transform.position;
+
+        // Aim
+        AimDirection = (owner.Target.position - transform.position).normalized;
+
+        ChangeState(State.Cast, ModifiedCastTime);
+        CastState(owner);
+    }
+
+    public override void AbilityUpdate(EnemyStateMachine owner)
+    {
+        if (currentState == State.Done) return;
+
+        stateTimer -= Time.deltaTime;
+        if (stateTimer <= 0f) StateTransition(owner, true);
+    }
+
+    public override void AbilityFixedUpdate(EnemyStateMachine owner)
+    {
+        if (currentState == State.Impact)
+        {
+            owner.EnemyRB.linearVelocity = AimDirection * slideForce;
+        }
+    }
+
+    public override void CastState(EnemyStateMachine owner)
+    {
+        AnimateEnemy(owner, skillType, State.Cast);
+        owner.EnemyAnimator.SetFloat("Horizontal", AimDirection.x);
+        owner.EnemyAnimator.SetFloat("Vertical", AimDirection.y);
+
+        owner.enemy.CastBar.StartCast(castTime: CastTime, owner.enemy.CurrentAttackSpeed);
+        Telegraph(false, false);
+    }
+
+    public override void ActionState(EnemyStateMachine owner)
+    {
+        owner.Buffs.phase.StartPhase(buffDuration);
+        owner.Buffs.immoveable.StartImmovable(buffDuration);
+
+        //AnimateEnemy(owner, skillType, State.Action);
+    }
+
+    public override void ImpactState(EnemyStateMachine owner)
+    {
+        AnimateEnemy(owner, skillType, State.Impact);
+        Attack(owner.NetworkObject, false, false);
+    }
+
+    public override void RecoveryState(EnemyStateMachine owner)
+    {
+        AnimateEnemy(owner, skillType, State.Recovery);
+        owner.enemy.CastBar.StartRecovery(RecoveryTime, owner.enemy.CurrentAttackSpeed);
+    }
+}
