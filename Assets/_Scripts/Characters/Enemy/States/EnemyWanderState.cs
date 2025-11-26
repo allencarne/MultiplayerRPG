ad1cf3a0ff8b2b19c857f7a8d7556ce983b8526e
allencarne <34139066+allencarne@users.noreply.github.com>
2025-11-26 10:40:15 -0800

    Updated Enemy Wander Pathing
    Now avoids picking wander positions out of bounds

--- Assets/_Scripts/Characters/Enemy/States/EnemyWanderState.cs
+++ Assets/_Scripts/Characters/Enemy/States/EnemyWanderState.cs
@@ -11,7 +11,7 @@ public class EnemyWanderState : EnemyState
 
         if (owner.IsServer)
         {
-            owner.WanderPosition = GetRandomClearPoint(owner.StartingPosition, owner.WanderRadius, owner.obstacleLayerMask);
+            owner.WanderPosition = GetRandomClearPoint(owner);
         }
 
         Vector2 direction = (owner.WanderPosition - (Vector2)owner.transform.position).normalized;
@@ -58,34 +58,30 @@ public class EnemyWanderState : EnemyState
         }
     }
 
-    Vector2 GetRandomClearPoint(Vector2 startingPosition, float maxRadius, LayerMask obstacleLayer, int maxAttempts = 10)
+    Vector2 GetRandomClearPoint(EnemyStateMachine owner)
     {
+        int maxAttempts = 10;
+
         for (int i = 0; i < maxAttempts; i++)
         {
-            // Get Random Point within Wander Radius
-            Vector2 randomPos = startingPosition + Random.insideUnitCircle * maxRadius;
-
-            // Get Direction to Point
+            Vector2 randomPos = owner.StartingPosition + Random.insideUnitCircle * owner.WanderRadius;
             Vector2 randomDir = (randomPos - (Vector2)transform.position);
-
-            // Get Distance to Point
             float distance = randomDir.magnitude;
 
-            // Raycast from Enemy Position
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, randomDir.normalized, distance, obstacleLayer);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, randomDir.normalized, distance, owner.obstacleLayerMask);
 
-            // Draw Line
-            Color rayColor = hit.collider == null ? Color.green : Color.red;
-            Debug.DrawLine(transform.position, randomPos, rayColor, 1f);
-
-            // If Valid Path is Found
-            if (hit.collider == null && !Physics2D.OverlapCircle(randomPos, .5f, obstacleLayer))
+            if (hit.collider != null)
+            {
+                randomPos = hit.point;
+            }
+            else
             {
                 return randomPos;
             }
+
+            Debug.DrawLine(transform.position, randomPos, Color.red, 1f);
         }
 
-        // If no valid position is found
-        return startingPosition;
+        return owner.StartingPosition;
     }
 }
