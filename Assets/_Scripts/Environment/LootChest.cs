f09ebf0014eaa12185578aa75092b7325d379d04
allencarne <34139066+allencarne@users.noreply.github.com>
2026-01-11 22:12:29 -0800

    Started Loot Chest

---
+++ Assets/_Scripts/Environment/LootChest.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.LowLevel.InputStateHistory;
+
+public class LootChest : MonoBehaviour, IInteractable
+{
+    [SerializeField] GetPlayerReference getPlayer;
+
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] Sprite closedSprite;
+    [SerializeField] Sprite openedSprite;
+
+    [SerializeField] string area;
+    [SerializeField] int index;
+    PlayerStats PlayerStats;
+
+    public int Coins;
+    public int MaxCoinReward;
+    [SerializeField] Item coin;
+    [SerializeField] Item[] rewards;
+
+    [SerializeField] int experienceReward;
+
+    public string DisplayName => "Loot Chest";
+
+    public void Initalize()
+    {
+        if (getPlayer != null)
+        {
+            PlayerStats = getPlayer.player.GetComponent<PlayerStats>();
+            if (PlayerStats == null) return;
+
+            PlayerStats.net_CharacterSlot.OnValueChanged += OnCharacterSlotChanged;
+            UpdateVisual(PlayerStats.net_CharacterSlot.Value);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (PlayerStats != null && PlayerStats.net_CharacterSlot != null)
+        {
+            PlayerStats.net_CharacterSlot.OnValueChanged -= OnCharacterSlotChanged;
+        }
+    }
+
+    private void OnCharacterSlotChanged(int previousValue, int newValue)
+    {
+        UpdateVisual(newValue);
+    }
+
+    public void UpdateVisual(int characterNumber)
+    {
+        string status = PlayerPrefs.GetString($"Character{characterNumber}_{area}_Chest_{index}", "Incomplete");
+
+        if (status == "Completed")
+        {
+            sprite.sprite = openedSprite;
+        }
+        else
+        {
+            sprite.sprite = closedSprite;
+        }
+    }
+
+    public void Interact(PlayerInteract player)
+    {
+        PlayerStats stats = player.GetComponentInParent<PlayerStats>();
+        if (stats == null || !stats.IsOwner) return;
+
+        int characterNumber = stats.net_CharacterSlot.Value;
+
+        string status = PlayerPrefs.GetString($"Character{characterNumber}_{area}_Chest_{index}", "Incomplete");
+
+        if (status == "Completed") return;
+
+        PlayerPrefs.SetString($"Character{characterNumber}_{area}_Chest_{index}", "Completed");
+        PlayerPrefs.Save();
+
+        sprite.sprite = openedSprite;
+
+        CoinReward();
+        ItemReward();
+        ExperienceReward();
+    }
+
+    void CoinReward()
+    {
+        int amount = Random.Range(0, Coins + 1);
+
+        for (int i = 0; i < amount; i++)
+        {
+            if (getPlayer.player != null)
+            {
+                Inventory inventory = getPlayer.player.GetComponentInChildren<Inventory>();
+                if (inventory != null) inventory.CoinCollected(amount);
+            }
+        }
+    }
+
+    void ItemReward()
+    {
+        foreach (Item reward in rewards)
+        {
+            if (Random.Range(0f, 100f) < reward.DropChance)
+            {
+                if (getPlayer.player != null)
+                {
+                    Inventory inventory = getPlayer.player.GetComponentInChildren<Inventory>();
+                    if (inventory != null) inventory.AddItem(reward);
+                }
+            }
+        }
+    }
+
+    public void ExperienceReward()
+    {
+        if (getPlayer.player != null)
+        {
+            PlayerExperience playerEXP = getPlayer.player.GetComponent<PlayerExperience>();
+            if (playerEXP != null) playerEXP.IncreaseEXP(experienceReward);
+        }
+    }
+}
