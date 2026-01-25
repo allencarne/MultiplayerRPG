2e86cbab30cbe9dbc7fdee34fb2c5d40d81b5cea
allencarne <34139066+allencarne@users.noreply.github.com>
2026-01-25 14:18:22 -0800

    Added World Map Zoom

--- Assets/_Scripts/UI/PlayerUI/WorldMap.cs
+++ Assets/_Scripts/UI/PlayerUI/WorldMap.cs
@@ -12,14 +12,22 @@ public class WorldMap : MonoBehaviour
     [Header("World Map Settings")]
     [SerializeField] RenderTexture worldMapRenderTexture;
     [SerializeField] RenderTexture miniMapRenderTexture;
-
-
     [SerializeField] Vector3 worldMapCameraPosition = new Vector3(200, 40, -10);
+
+    [Header("Zoom Settings")]
     [SerializeField] float worldMapZoom = 125;
+    [SerializeField] Vector2 baseImageSize = new Vector2(640, 360);
+    RectTransform mapRectTransform;
+    private float currentZoomLevel = 1f;
 
     Vector3 cameraPosition;
     float cameraZoom;
     Transform originalParent;
+
+    private void Awake()
+    {
+        mapRectTransform = map.GetComponent<RectTransform>();
+    }
 
     private void OnEnable()
     {
@@ -46,6 +54,8 @@ public class WorldMap : MonoBehaviour
         mapCamera.targetTexture = worldMapRenderTexture;
         mapCamera.transform.position = worldMapCameraPosition;
         mapCamera.orthographicSize = worldMapZoom;
+
+        SetZoomLevel(1f);
     }
 
     public void EnableMiniMap()
@@ -59,5 +69,37 @@ public class WorldMap : MonoBehaviour
         mapCamera.targetTexture = miniMapRenderTexture;
         mapCamera.transform.position = cameraPosition;
         mapCamera.orthographicSize = cameraZoom;
+    }
+
+    public void SetZoom50()
+    {
+        SetZoomLevel(0.5f);
+    }
+
+    public void SetZoom75()
+    {
+        SetZoomLevel(0.75f);
+    }
+
+    public void SetZoom100()
+    {
+        SetZoomLevel(1f);
+    }
+
+    public void SetZoom125()
+    {
+        SetZoomLevel(1.25f);
+    }
+
+    public void SetZoom150()
+    {
+        SetZoomLevel(1.50f);
+    }
+
+    private void SetZoomLevel(float zoomLevel)
+    {
+        currentZoomLevel = zoomLevel;
+        Vector2 newSize = baseImageSize * zoomLevel;
+        mapRectTransform.sizeDelta = newSize;
     }
 }
