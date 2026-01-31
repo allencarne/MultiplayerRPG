668d1dce24f6c3a93078a7bf0b9d865b7dafb6a9
allencarne <34139066+allencarne@users.noreply.github.com>
2026-01-30 21:38:38 -0800

    Disabled scene camera on start

--- Assets/_Scripts/Camera/CameraFollow.cs
+++ Assets/_Scripts/Camera/CameraFollow.cs
@@ -5,6 +5,15 @@ public class CameraFollow : MonoBehaviour
     public Transform playerTransform;
     public float smoothSpeed;
 
+    private void Start()
+    {
+        Camera sceneCamera = GameObject.Find("Scene Camera").GetComponent<Camera>();
+        if (sceneCamera != null)
+        {
+            sceneCamera.enabled = false;
+        }
+    }
+
     private void LateUpdate()
     {
         if (playerTransform != null)
