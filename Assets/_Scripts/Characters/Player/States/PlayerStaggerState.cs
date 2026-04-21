f9f71681dba6f780430542ef97225c14bc831c05
allencarne <34139066+allencarne@users.noreply.github.com>
2026-04-21 08:21:44 -0700

    Player Stagger State Updated

--- Assets/_Scripts/Characters/Player/States/PlayerStaggerState.cs
+++ Assets/_Scripts/Characters/Player/States/PlayerStaggerState.cs
@@ -5,7 +5,17 @@ public class PlayerStaggerState : PlayerState
     {
         if (!owner.IsOwner) return;
 
-        owner.BodyAnimator.Play("Stagger");
+        owner.PlayerHeadAnimator.Play("Stagger", -1, 0);
+        owner.BodyAnimator.Play("Stagger", -1, 0);
+
+        // Hide Clothes, Eyes, & Hair?
+        //owner.ChestAnimator.Play("Stagger_" + owner.customization.net_ChestIndex.Value, -1, 0);
+        //owner.LegsAnimator.Play("Stagger_" + owner.customization.net_LegsIndex.Value, -1, 0);
+
+        if (owner.Equipment.IsWeaponEquipped)
+        {
+            owner.WeaponAnimator.Play(owner.customization.WeaponAnimType + " Stagger", -1, 0);
+        }
     }
 
     public override void UpdateState(PlayerStateMachine owner)
