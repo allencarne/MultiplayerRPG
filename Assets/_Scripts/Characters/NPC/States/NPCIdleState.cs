f9aadcad6ae330567023c2ac52a7c4c5564232ff
allencarne <allencarne@yahoo.com>
2026-07-24 21:34:42 -0700

    Updated Guard Appearances

--- Assets/_Scripts/Characters/NPC/States/NPCIdleState.cs
+++ Assets/_Scripts/Characters/NPC/States/NPCIdleState.cs
@@ -4,24 +4,18 @@ public class NPCIdleState : NPCState
 {
     public override void StartState(NPCStateMachine owner)
     {
+        // Face Down
+        Vector2 dir = new Vector2 (0, -1);
+        owner.SetAnimDir(dir);
+        owner.npc.npcHead.SetEyes(dir);
+        owner.npc.npcHead.SetHair(dir);
+        owner.npc.npcHead.SetHelm(dir);
 
-        owner.HeadAnimator.SetFloat("Vertical", -1);
-        owner.BodyAnimator.SetFloat("Vertical", -1);
-
-        owner.ChestAnimator.SetFloat("Vertical", -1);
-        owner.LegsAnimator.SetFloat("Vertical", -1);
-
-        owner.SwordAnimator.SetFloat("Vertical", -1);
-
-
-
+        // Play Animation
         owner.HeadAnimator.Play("Idle", -1, 0);
         owner.BodyAnimator.Play("Idle", -1, 0);
-
         owner.ChestAnimator.Play("Idle_" + owner.npc.Data.ChestIndex, -1, 0);
         owner.LegsAnimator.Play("Idle_" + owner.npc.Data.LegsIndex, -1, 0);
-
-        // Add Idle Index
         owner.SwordAnimator.Play(owner.npc.Data.WeaponType.ToString() + " Idle", -1, 0);
     }
 
