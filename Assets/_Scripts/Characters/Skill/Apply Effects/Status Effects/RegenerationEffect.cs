110563f95ce877291b9b37e8d6f51434f26329f2
allencarne <allencarne@yahoo.com>
2026-08-28 13:43:29 -0700

    Added Regeneration Effect

---
+++ Assets/_Scripts/Characters/Skill/Apply Effects/Status Effects/RegenerationEffect.cs
@@ -0,0 +1,17 @@
+using Unity.Netcode;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Skill/Skill Effects/Status Effect/RegenerationEffect")]
+public class RegenerationEffect : ApplyEffect
+{
+    public int Stacks = 1;
+    public float Duration;
+
+    protected override void ApplyTo(NetworkObject target, PlayerStateMachine owner, SkillContext ctx)
+    {
+        Buffs buffs = target.GetComponent<Buffs>();
+        if (buffs == null) return;
+
+        buffs.regeneration.StartRegen(Stacks, Duration);
+    }
+}
